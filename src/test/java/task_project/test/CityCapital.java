22a2beac39bedb0805fd0592ecff09afe85ea9ee
roma3 <roma38lorde@gmail.com>
2022-08-22 15:29:16 -0400

    Home task

--- src/test/java/task_project/test/CityCapital.java
+++ src/test/java/task_project/test/CityCapital.java
@@ -5,7 +5,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.*;
